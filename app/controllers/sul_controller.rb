c0b82109d6d6059da2522f826227665653263e32
Geonwoo <dpel959@naver.com>
2017-05-15 07:01:19 +0000

    MVC&버튼지정

--- app/controllers/sul_controller.rb
+++ app/controllers/sul_controller.rb
@@ -1,5 +1,7 @@
 class SulController < ApplicationController
     def index
         @foods = ['피자', '탕수육', '치킨', '소주', '라면', '불백']
+        @weapons = ['빠루', '파이프', '도끼', '총', '칼']
+        @goods = ['상자', '휴대폰', '배터리', '빨대', '커피', '노트북']
     end
 end
